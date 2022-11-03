daa21acc5a6671dc48e9c23d96b41ceee957d804
Rizky_Yudha <102364151+Rzydx@users.noreply.github.com>
2022-11-03 10:25:41 +0700

    Update language.py

--- PrimeMusic/plugins/tools/language.py
+++ PrimeMusic/plugins/tools/language.py
@@ -15,8 +15,8 @@ from PrimeMusic.utils.decorators import (ActualAdminCB, language,
 def lanuages_keyboard(_):
     keyboard = InlineKeyboard(row_width=2)
     keyboard.row(
-        InlineKeyboardButton(text="🏴󠁧󠁢󠁥󠁮󠁧󠁿 English", callback_data=f"languages:en"),
-        InlineKeyboardButton(text="🇮🇩 Indonesia", callback_data=f"languages:id"),
+        InlineKeyboardButton(text="🏴󠁧󠁢󠁥󠁮󠁧󠁿 English", callback_data=f"languages:id"),
+        InlineKeyboardButton(text="🇮🇩 Indonesia", callback_data=f"languages:en"),
     )
     keyboard.row(
         InlineKeyboardButton(text="🇮🇩 Jawa", callback_data=f"languages:jawa"),
