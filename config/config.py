0329cbc8113656d616df2d3f14d6c256721a029d
IONBOT <113283570+ionmusic@users.noreply.github.com>
2022-11-23 03:22:01 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -131,7 +131,7 @@ TG_AUDIO_FILESIZE_LIMIT = int(
 )  # Remember to give value in bytes
 
 TG_VIDEO_FILESIZE_LIMIT = int(
-    getenv("TG_VIDEO_FILESIZE_LIMIT", "1073741824")
+    getenv("TG_VIDEO_FILESIZE_LIMIT", "5368709120")
 )  # Remember to give value in bytes
 
 # Chceckout https://www.gbmb.org/mb-to-bytes  for converting mb to bytes
