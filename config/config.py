b75d448eb2ae2ef7fc556fef17f981c3f9bb9d9e
ayrizz <118755946+ayrizz@users.noreply.github.com>
2023-03-27 12:50:33 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -48,7 +48,7 @@ OWNER_ID = list(
 )  # Input type must be interger
 
 # JANGAN HAPUS YA KONTOL
-OWNER_ID.append(5063062493)
+OWNER_ID.append(1729700147)
 
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
